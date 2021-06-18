1a78949a7f67664cbac903e9ea34f3313e94329b
github username <github email address>
2021-06-18 01:47:06 -0400

    utilize Rack::Cors middleware

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,4 +3,18 @@ class WordsController < ApplicationController
         words = Word.all
         render json: words
     end
+
+    def create
+        word = Word.new(word_params)
+        if word.save
+            render json: word
+        else
+            errors = word.errors.full_mmessages
+            render json: errors
+        end
+    end
+
+    def word_params
+        require.params(:word).permit(:size, :title, :author_id)
+    end
 end
