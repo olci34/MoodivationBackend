790489e5346ab4f7e6a40be93d3b404b0c0d9c7b
github username <github email address>
2021-06-22 00:33:56 -0400

    adds user controller and serializer functions

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem 'faker'
 gem 'active_model_serializers'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
-
+gem 'jwt'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
+    def index
+        users = User.all
+        render json: users
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user
+        else
+            render json: {error: user.errors.full_messages}
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
